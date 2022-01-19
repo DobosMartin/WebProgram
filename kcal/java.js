72c886842a9c3ac03c8a9194958d0581529b7962
DobosMartin <84835783+DobosMartin@users.noreply.github.com>
2022-01-19 19:32:02 +0100

    Add files via upload

---
+++ kcal/java.js
@@ -0,0 +1,13 @@
+function szamol()
+{
+    document.kalk.el.value=Math.round(document.kalk.m1.value * 680/60);
+    document.kalk.el2.value=Math.round(document.kalk.m2.value * 680/60);
+    document.kalk.el3.value=Math.round(document.kalk.m3.value * 680/60);
+    document.kalk.el4.value=Math.round(document.kalk.m4.value * 680/60);
+    document.kalk.el5.value=Math.round(document.kalk.m5.value * 680/60);
+    document.kalk.el6.value=Math.round(document.kalk.m6.value * 680/60);
+    document.kalk.el7.value=Math.round(document.kalk.m7.value * 680/60);
+    document.kalk.el8.value=Math.round(document.kalk.m8.value * 680/60);
+    document.kalk.el9.value=Math.round(document.kalk.m9.value * 680/60);
+    document.kalk.el10.value=Math.round(document.kalk.m10.value * 680/60);
+}
