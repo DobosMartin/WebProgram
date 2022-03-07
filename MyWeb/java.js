65bcc0cd3192a22ed1da5ff82e0b4e94d7d898db
DobosMartin <84835783+DobosMartin@users.noreply.github.com>
2022-03-07 22:42:41 +0100

    drop

--- MyWeb/java.js
+++ MyWeb/java.js
@@ -1,7 +1,12 @@
 function Belepes(){
     var nev= document.getElementById('firstName').value ;
     var jelszo = document.getElementById('password').value;
+    if(nev == "admin" ||jelszo=="a" ){
         document.location.href = "main.html";
+    }
+    else{
+        alert('nem,nem');
+    }
    
 
 }
